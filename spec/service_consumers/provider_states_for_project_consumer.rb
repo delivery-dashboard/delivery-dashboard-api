cc7849b97629d30706b4690a68d891f711dec4ae
Bruno Dias <bdias@thoughtworks.com>
2015-07-09 09:57:38 -0300

    Setup pact fixtures.

--- spec/service_consumers/provider_states_for_project_consumer.rb
+++ spec/service_consumers/provider_states_for_project_consumer.rb
@@ -2,12 +2,16 @@ Pact.provider_states_for 'Projects Consumer' do
 
   provider_state "at least one project exists" do
     set_up do
+      Project.destroy_all
+      Project.create(name: "P1")
+      Project.create(name: "P2")
       # Create a thing here using your framework of choice
       # eg. Sequel.sqlite[:somethings].insert(name: "A small something")
     end
 
     tear_down do
       # Any tear down steps to clean up your code
+      Project.destroy_all
     end
   end
 end
