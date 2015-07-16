38deeec4140783334c1b1782843fb36aa215c034
Bruno Dias <bdias@thoughtworks.com>
2015-07-16 09:37:05 -0300

    Set pacts folder.

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -4,6 +4,6 @@ require './spec/service_consumers/provider_states_for_project_consumer.rb'
 
 Pact.service_provider 'Projects Provider' do
   honours_pact_with 'UI' do
-    pact_uri '../delivery-dashboard-ui/tmp/pacts/projects_consumer-projects_provider.json'
+    pact_uri '/tmp/pacts/projects_consumer-projects_provider.json'
   end
 end
